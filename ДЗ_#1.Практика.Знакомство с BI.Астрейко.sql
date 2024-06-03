817dd22b9c7435623b839dd1f74c780ea644ad88
NataAstreiko <nata.astreiko@icloud.com>
2024-06-03 14:35:54 +0300

    Add files via upload

---
+++ ДЗ_#1.Практика.Знакомство с BI.Астрейко.sql
@@ -0,0 +1,23 @@
+a)
+SELECT *
+FROM Sales.SalesTerritory
+
+b)
+SELECT TerritoryID, Name
+FROM Sales.SalesTerritory
+
+c)
+SELECT *
+FROM Person.Person
+WHERE LastName='Norman'
+
+d)
+SELECT *
+FROM Person.Person
+WHERE EmailPromotion!=2
+
+OR
+
+SELECT *
+FROM Person.Person
+WHERE EmailPromotion<>2
