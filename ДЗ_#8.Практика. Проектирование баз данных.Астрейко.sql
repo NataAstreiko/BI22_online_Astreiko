cb6b478435e529e584b8b38862386b028e0dcd82
NataAstreiko <nata.astreiko@icloud.com>
2024-07-08 20:19:22 +0300

    Add files via upload

--- ДЗ_#8.Практика. Проектирование баз данных.Астрейко.sql
+++ ДЗ_#8.Практика. Проектирование баз данных.Астрейко.sql
@@ -1,9 +1,9 @@
-
+﻿
 SELECT DISTINCT pp.Name as ProductName, pp.ProductNumber,coalesce(pp.Class,'n/d') as Class,
 CASE
 WHEN pp.Size LIKE '%[a-z]%' THEN 'Lateral'
 WHEN pp.Size LIKE '%[0-9]%' THEN 'Numeric'
-WHEN pp.Size IS NULL THEN 'Unknown' --èëè ELSE (íà ñëó÷àé, åñëè êòî-òî ïîñòàâèë ïðî÷åðê, à íå íè÷åãî)
+WHEN pp.Size IS NULL THEN 'Unknown' --или ELSE (на случай, если кто-то поставил прочерк, а не ничего)
 END as Product_size,
 coalesce(first_value(pv.Name) over (partition by pp.Name order by ppv.lastReceiptDate), 'n/d') as VendorName,
 coalesce(ppm.Name , 'n/d') as ModelName,
@@ -18,7 +18,7 @@ on pv.BusinessEntityID=ppv.BusinessEntityID
 and pv.CreditRating = 1
 and pv.ActiveFlag = 1
 
-left join (select  pod.ProductID, avg(pod.UnitPrice) as AvgUnitPrice ---êàê èñïîëíÿåòñÿ "çàïðîñ äîëæåí âûïîëíÿòüñÿ íå áîëåå 10 ñåêóíä"?
+left join (select  pod.ProductID, avg(pod.UnitPrice) as AvgUnitPrice ---как исполняется "запрос должен выполняться не более 10 секунд"?
 			from Purchasing.PurchaseOrderHeader as poh
 			join Purchasing.PurchaseOrderDetail as pod
 			on poh.PurchaseOrderID = pod.PurchaseOrderID
